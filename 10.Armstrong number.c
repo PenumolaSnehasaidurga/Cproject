239796d43e115f899626eda24930b5c169394862
PenumolaSnehasaidurga <penumolasnehasaidurga@gmail.com>
2024-10-15 17:40:07 +0530

    Update 10.Armstrong number.c

--- 10.Armstrong number.c
+++ 10.Armstrong number.c
@@ -1,22 +1,39 @@
 //Aim:Write a C program to whether the given number is Armstrong Number of Not.
-#include<stdio.h>
+include<stdio.h>
 #include<math.h>
- main()
+main()
 {
-int r,n,t,s=0;
-printf("Enter number:");
+int n,l=0,t,v,r,s=0;
+printf("Enter your number");
 scanf("%d",&n);
 t=n;
+v=t;
 while(n>0)
 {
-r=n%10;
-s=s+pow(r,3);
-n=n/10;
+ n=n/10;
+ l++;
 }
-if(s==t)
+for(;t>0;t=t/10)
 {
-  printf("%d is a Armstrong number",t);
+  r=t%10;
+  s=s+pow(r,l);
+}
+if(s==v)
+{
+  printf("%d is an Armstrong number",v);
 }
 else
-   printf("%d is not a Armstrong number",t);
+  printf("%d is not an Armstrong number",v);
 }
+
+
+
+
+
+
+
+
+
+
+
+    
