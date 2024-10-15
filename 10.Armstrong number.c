5cf456cb84d39c76c579be41388ed9f20f472f42
PenumolaSnehasaidurga <penumolasnehasaidurga@gmail.com>
2024-10-15 10:28:16 +0530

    Update 10.Armstrong number.c

--- 10.Armstrong number.c
+++ 10.Armstrong number.c
@@ -3,14 +3,20 @@
 #include<math.h>
  main()
 {
-int r,n,s=0;
+int r,n,t,s=0;
 printf("Enter number:");
 scanf("%d",&n);
+t=n;
 while(n>0)
 {
 r=n%10;
 s=s+pow(r,3);
 n=n/10;
 }
-printf("%d is the Armstrong number",s);
+if(s==t)
+{
+  printf("%d is a Armstrong number",t);
+}
+else
+   printf("%d is not a Armstrong number",t);
 }
